4e68c7b7dceae766ac5c85a5ed7b487d608d694b
gachavesb <53975008+gachavesb@users.noreply.github.com>
2021-10-21 10:30:09 -0500

    Update create-container.sh

--- terraform-aks-master/1-prerequisites/create-container.sh
+++ terraform-aks-master/1-prerequisites/create-container.sh
@@ -1,14 +1,11 @@
 # Create general purpose V2 storage account
 
-accountName=$(terraformstorageaccount)
-rg=$(terraformstoragerg)
+az group create --name $(terraformstoragerg) --location eastus --output json
 
-az group create --name $rg --location eastus --output json
+az storage account create --name $(terraformstorageaccount) --resource-group $(terraformstoragerg) --location eastus --sku Standard_RAGRS --kind StorageV2
 
-az storage account create --name $accountName --resource-group $rg --location eastus --sku Standard_RAGRS --kind StorageV2
+accountKey=$(az storage account keys list --account-name $(terraformstorageaccount) --resource-group $(terraformstoragerg) --query "[?keyName == 'key1'].value" -o tsv)
 
-accountKey=$(az storage account keys list --account-name $accountName --resource-group $rg --query "[?keyName == 'key1'].value" -o tsv)
+az storage container create -n tfstate --account-name $(terraformstorageaccount) --account-key $accountKey
 
-az storage container create -n tfstate --account-name $accountName --account-key $accountKey
-
-terraform init -backend-config="storage_account_name=$accountName" -backend-config="container_name=tfstate" -backend-config="access_key=$accountKey" -backend-config="key=dev.tfstate"
+terraform init -backend-config="storage_account_name=$(terraformstorageaccount)" -backend-config="container_name=tfstate" -backend-config="access_key=$accountKey" -backend-config="key=dev.tfstate"
